02023236db62a582a82c8dc43bd9c6b3982a4019
Saul <phasecross@hotmail.com>
2022-11-23 17:06:44 -0700

    Modified all routes so they always return a response and use db.query

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,158 +1,153 @@
 const { response } = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
+const db = require("../services/db");
 const config = require("../config");
 
 const connection = mysql.createConnection(config.db);
 //GET Minesweeper
-exports.getMinesweeper = (request, response) => {
-  connection.query(
-    "SELECT name_ms, score_ms FROM kubergames.sb_minesweeper ORDER BY score_ms ASC LIMIT 10;",
-    function (error, results) {
-      if (error) throw error;
+exports.getMinesweeper = async (request, response) => {
+  try {
+    const rows = await db.query(
+      "SELECT name_ms, score_ms FROM kubergames.sb_minesweeper ORDER BY score_ms ASC LIMIT 10;"
+    );
 
-      response.json({
-        results: results.map((item, index) => {
-          return {
-            name: item.name_ms,
-            score: item.score_ms,
-          };
-        }),
-      });
-    }
-  );
+    response.status(200).json({ results: rows });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+    });
+  }
 };
 
 //POST Minesweeper
-exports.addMinesweeper = (request, response) => {
+exports.addMinesweeper = async (request, response) => {
   const { name, score } = request.body;
-  //console.log(name, score);
-  connection.query(
-    `INSERT INTO sb_minesweeper(name_ms, score_ms) VALUES ('${name}',${score})`,
-    function (error, results) {
-      if (error) {
-        response.status(500).json({
-          message: error.message,
-        });
-      }
-      response.json({
-        count: results.affectedRows,
-      });
-    }
-  );
+
+  try {
+    const result = await db.query(
+      `INSERT INTO sb_minesweeper(name_ms, score_ms) VALUES ('${name}',${score})`
+    );
+
+    response.status(201).json({
+      insertedId: result.insertedId,
+      affectedRows: result.affectedRows,
+      message: "Score submitted successfully!",
+    });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+      code: error.code,
+    });
+  }
 };
 
 //GET Space Invaders
-exports.getSpaceInvaders = (request, response) => {
-  connection.query(
-    "SELECT name_si, score_si FROM kubergames.sb_space_invaders ORDER BY score_si DESC LIMIT 10;",
-    function (error, results) {
-      if (error) throw error;
+exports.getSpaceInvaders = async (request, response) => {
+  try {
+    const rows = await db.query(
+      "SELECT name_si, score_si FROM kubergames.sb_space_invaders ORDER BY score_si DESC LIMIT 10;"
+    );
 
-      response.json({
-        results: results.map((item, index) => {
-          return {
-            name: item.name_si,
-            score: item.score_si,
-          };
-        }),
-      });
-    }
-  );
+    response.json({ results: rows });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+    });
+  }
 };
 
 //POST Space Invaders
-exports.addSpaceInvaders = (request, response) => {
+exports.addSpaceInvaders = async (request, response) => {
   const { name, score } = request.body;
-  //console.log(name, score);
-  connection.query(
-    `INSERT INTO sb_space_invaders(name_si, score_si) VALUES ('${name}',${score})`,
-    function (error, results) {
-      if (error) {
-        response.status(500).json({
-          message: error.message,
-        });
-      }
-      response.json({
-        count: results.affectedRows,
-      });
-    }
-  );
+
+  try {
+    const result = await db.query(
+      `INSERT INTO sb_space_invaders(name_si, score_si) VALUES ('${name}',${score})`
+    );
+
+    response.status(201).json({
+      insertedId: result.insertedId,
+      affectedRows: result.affectedRows,
+      message: "Score submitted successfully!",
+    });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+      code: error.code,
+    });
+  }
 };
 
 //GET Snake Game
-exports.getSnakeGame = (request, response) => {
-  connection.query(
-    "SELECT name_sg, score_sg FROM kubergames.sb_snake_game ORDER BY score_sg DESC LIMIT 10;",
-    function (error, results) {
-      if (error) throw error;
+exports.getSnakeGame = async (request, response) => {
+  try {
+    const rows = await db.query(
+      "SELECT name_sg, score_sg FROM kubergames.sb_snake_game ORDER BY score_sg DESC LIMIT 10;"
+    );
 
-      response.json({
-        results: results.map((item, index) => {
-          return {
-            name: item.name_sg,
-            score: item.score_sg,
-          };
-        }),
-      });
-    }
-  );
+    response.json({ results: rows });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+    });
+  }
 };
 
 //POST Snake Game
-exports.addSnakeGame = (request, response) => {
+exports.addSnakeGame = async (request, response) => {
   const { name, score } = request.body;
-  //console.log(name, score);
-  connection.query(
-    `INSERT INTO sb_snake_game(name_sg, score_sg) VALUES ('${name}',${score})`,
-    function (error, results) {
-      if (error) {
-        response.status(500).json({
-          message: error.message,
-        });
-      }
-      response.json({
-        count: results.affectedRows,
-      });
-    }
-  );
+
+  try {
+    const result = await db.query(
+      `INSERT INTO sb_snake_game(name_sg, score_sg) VALUES ('${name}',${score})`
+    );
+
+    response.status(201).json({
+      insertedId: result.insertedId,
+      affectedRows: result.affectedRows,
+      message: "Score submitted successfully!",
+    });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+      code: error.code,
+    });
+  }
 };
 
 //GET Flappy Bird
-exports.getFlappyBird = (request, response) => {
-  connection.query(
-    "SELECT name_fb, score_fb FROM kubergames.sb_flappy_bird ORDER BY score_fb DESC LIMIT 10;",
-    function (error, results) {
-      if (error) throw error;
+exports.getFlappyBird = async (request, response) => {
+  try {
+    const rows = await db.query(
+      "SELECT name_fb, score_fb FROM kubergames.sb_flappy_bird ORDER BY score_fb DESC LIMIT 10;"
+    );
 
-      response.json({
-        results: results.map((item, index) => {
-          return {
-            name: item.name_fb,
-            score: item.score_fb,
-          };
-        }),
-      });
-    }
-  );
+    response.json({ results: rows });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+    });
+  }
 };
 
 //POST Flappy Bird
-exports.addFlappyBird = (request, response) => {
+exports.addFlappyBird = async (request, response) => {
   const { name, score } = request.body;
-  //console.log(name, score);
-  connection.query(
-    `INSERT INTO sb_flappy_bird(name_fb, score_fb) VALUES ('${name}',${score})`,
-    function (error, results) {
-      if (error) {
-        response.status(500).json({
-          message: error.message,
-        });
-      }
-      response.json({
-        count: results.affectedRows,
-      });
-    }
-  );
+
+  try {
+    const result = await db.query(
+      `INSERT INTO sb_flappy_bird(name_fb, score_fb) VALUES ('${name}',${score})`
+    );
+
+    response.status(201).json({
+      insertedId: result.insertedId,
+      affectedRows: result.affectedRows,
+      message: "Score submitted successfully!",
+    });
+  } catch (error) {
+    response.status(500).json({
+      error: error.message,
+      code: error.code,
+    });
+  }
 };
-
-
