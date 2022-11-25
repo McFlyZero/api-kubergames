51fa3ac5feb6da3b6906debc9b7f7318e33ab0fa
Saul <phasecross@hotmail.com>
2022-11-24 20:34:09 -0700

    Added response headers

--- api.js
+++ api.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const GamesRoute = require("./routes/games");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const GamesRoute = require("./routes/games");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/kubergames", GamesRoute);
+// console.log(process.env);
 
 app.listen(3000, () => {
   console.log("server on port 3000");
